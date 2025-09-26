4ba22817cc1d0b7cdd026cc02a7f48249010b018
aravindhshankar <aravindhswaminathan@gmail.com>
2025-09-26 19:58:07 +0200

    modified parseCouplings according to pairs specifications

--- python/parseCouplings.py
+++ python/parseCouplings.py
@@ -1,15 +1,24 @@
 import numpy as np
 import os
 import csv
-from utils.helpers import makeJmat
+from utils.helpers import makeJpairs
 
 def parseCouplings(filename='couplings69.csv', M=4, N=4, d=2, BC='PBC', useH=True):
+    '''
+    reads couplings file, generates a list of Nsamples elements. 
+    Each element of the list contains 3 objects: Jijalphabeta, h and bonds.
+    Jijalphabeta is a list of 3x3 elements of size len(bonds)
+    bonds is a list that contains the connecting elements of the graph
+    h is a list of size number of sites = M * N
+    BC can be both PBC and OBC, turning useH = False puts all magnetic fields to zero
+    '''
     with open(filename, 'r') as f:
         reader = csv.reader(f)
-        return np.array([makeJmat(couplings, M, N, d, BC, useH) for couplings in reader], dtype=object).T
+        return np.array([makeJpairs(couplings, M, N, d, BC, useH) for couplings in reader], dtype=object).T
 
 def main():
-    Jlist, hlist = parseCouplings()
+    Jijalphabeta, h, bonds = parseCouplings()
+    print(bonds)
 
 if __name__ == '__main__':
     main()
--- python/utils/helpers.py
+++ python/utils/helpers.py
@@ -1,32 +1,83 @@
 import numpy as np
+import csv
 
 
-def makeJmat(couplings, M=4,N=4,d=2,BC='PBC',useH=True):
+def makeJmat(couplings, M=4, N=4, d=2, BC='PBC', useH=True):
     ''' acts on one line of couplings.csv and converts it to the required tensor '''
 
     if BC == 'PBC':
         total_bonds = M * N * d
         total_sites = M * N
-        shape = (M,N,3,3)
+        shape = (M, N, 3, 3)
     elif BC == 'OBC':
         total_bonds = (M-1) * (N-1) * d
         total_sites = N * N
-        shape = (M-1,N-1,3,3)
+        shape = (M-1, N-1, 3, 3)
     else:
-        raise(Exception("invalid choice of boundary condition: choices PBC, OBC "))
-    
+        raise (Exception("invalid choice of boundary condition: choices PBC, OBC "))
 
     Js = np.array(couplings[:-3])
-    assert len(Js)==3*3*d, 'Not the correct number of Js, splitting of couplings improperly done!'
-    Js = Js.reshape((d,3,3))
+    assert len(
+        Js) == 3*3*d, 'Not the correct number of Js, splitting of couplings improperly done!'
+    Js = Js.reshape((d, 3, 3))
     hs = np.array(couplings[-3:])
-    assert len(hs)==3, 'Not the correct number of hs, splitting of couplings improperly done!'
+    assert len(
+        hs) == 3, 'Not the correct number of hs, splitting of couplings improperly done!'
 
-    Jmat = np.zeros((total_bonds,3,3))
-    hmat = np.zeros((total_sites,3))
+    Jmat = np.zeros((total_bonds, 3, 3))
+    hmat = np.zeros((total_sites, 3))
     Jmat[0:total_bonds//d] = Js[0]
     Jmat[total_bonds//d:] = Js[1]
     if useH == True:
         hmat[:] = hs
 
     return [Jmat, hmat]
+
+def makeJpairs(couplings, M=4, N=4, d=2, BC='PBC', useH=True):
+    ''' for the format needed by construct_hamiltonian_bonds'''
+    total_sites = M * N
+    if BC == 'PBC':
+        total_bonds = M * N * d
+        shape = (M, N, 3, 3)
+    elif BC == 'OBC':
+        total_bonds = (M-1) * (N-1) * d
+        shape = (M-1, N-1, 3, 3)
+    else:
+        raise (Exception("invalid choice of boundary condition: choices PBC, OBC "))
+    Js = np.array(couplings[:-3])
+    assert len(
+        Js) == 3*3*d, 'Not the correct number of Js, splitting of couplings improperly done!'
+    Js = Js.reshape((d, 3, 3))  # J[0] = Jx, J[1] = Jy
+    hs = np.array(couplings[-3:])
+    assert len(
+        hs) == 3, 'Not the correct number of hs, splitting of couplings improperly done!'
+
+    ### OBC ###
+    ybonds = [(i, i+1) for i in range(total_sites) if (i+1) % M != 0]
+    xbonds = [(i, i+M) for i in range(total_sites) if (i+M) < total_sites]
+    if BC == 'PBC':
+        ### PBC contains additional bonds ###
+        xper = [(i, i % N)
+                for i in range(total_sites - M, total_sites)]  # note order
+        yper = [(i, i+1-M) for i in range(M-1, total_sites, N)]  # note order
+        # bonds = [pair for pair in [xbonds + xper + ybonds + yper]]
+        bonds = xbonds + xper + ybonds + yper
+    else:
+        # bonds = [pair for pair in [xbonds + ybonds]]
+        bonds = xbonds + ybonds
+
+    assert len(bonds) == total_bonds, print(f'ERROR IN BONDS CREATION!!!!! {len(bonds), total_bonds}')
+    Jmat = np.zeros((total_bonds, 3, 3))
+    hmat = np.zeros((total_sites, 3))
+    Jmat[0:total_bonds//d] = Js[0]
+    Jmat[total_bonds//d:] = Js[1]
+    if useH == True:
+        hmat[:] = hs
+    return (Jmat, hmat, bonds)
+
+
+if __name__ == '__main__':
+    with open('testcouplings.csv', 'r') as f:
+        couplings = [np.float64(cval) for cval in f.readline().split(',')]
+    result = makeJpairs(couplings)
+    print(result)
