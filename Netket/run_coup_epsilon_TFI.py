ee1b400b64ac5fe5704962f6298c24050252807b
Nils Niggemann <nils.niggemann@fu-berlin.de>
2025-09-28 13:56:46 +0200

    rand_coups

--- Netket/run_coup_epsilon_TFI.py
+++ Netket/run_coup_epsilon_TFI.py
@@ -14,16 +14,24 @@ input_file = "../python/couplings69.csv"
 # Set up the 2D lattice for TFI
 g = nk.graph.Hypercube(length=4, n_dim=2, pbc=True)
 
+def TFI2D(g, J=1.0, h=1.0):
+    hi = nk.hilbert.Spin(s=0.5, N=g.n_nodes)
+    H = -J * sum(nk.operator.spin.sigmaz(hi, i) * nk.operator.spin.sigmaz(hi, j) for (i, j) in g.edges())
+    H += -h * sum(nk.operator.spin.sigmax(hi, i) for i in g.nodes())
+    return H
+
 # Load couplings
 Jij, h, bonds = parseCouplings.parseCouplings(input_file)
 indices = np.random.choice(len(h), size=100, replace=False)
 
 for idx_num, i in enumerate(indices):
+    epsilon = np.random.uniform(0.01, 0.3)
     H_ran = construct_hamiltonian_bonds(Jij[i], h[i], bonds[i])
-    H = H_ran
+    H2 = TFI2D(g, J=1.0, h=0.5)
+    H = epsilon * H_ran + H2
 
     exact_ground_energy, exact_ground_state = nk.exact.lanczos_ed(H, k=1, compute_eigenvectors=True)
-    print(f"Hamiltonian {idx_num+1}: index={i}, Exact ground state energy: {exact_ground_energy[0]}")
+    print(f"Hamiltonian {idx_num+1}: index={i}, epsilon={epsilon:.3f}, Exact ground state energy: {exact_ground_energy[0]}")
 
     params = generate_params(
         alpha=1,
@@ -31,9 +39,10 @@ for idx_num, i in enumerate(indices):
         learning_rate=3e-2,
         n_iter=1000,
         show_progress=True,
-        out=f"data_rand/rbm_optimization_{i}",
+        out=f"data_rand_epsilon_TFI/rbm_optimization_{i}_eps_{epsilon:.3f}",
+        epsilon=epsilon,
     )
-    output_file = f"data_rand/rbm_optimization_{i}.npz"
+    output_file = f"data_rand/rbm_optimization_{i}_eps_{epsilon:.3f}.npz"
     if os.path.exists(output_file):
         print(f"Skipping Hamiltonian {idx_num+1}: index={i}, output file already exists.")
         continue
